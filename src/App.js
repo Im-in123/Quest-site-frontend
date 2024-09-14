8ca1a5633363a7621eb2d81821ef39b54488c637
Im-in123 <rudyelisha77@gmail.com>
2024-09-14 13:48:57 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { ToastContainer  } from "react-toastify";
 import './App.css';
+
 import AppRouter from './AppRouter';
  function App() {
   return (
     <div className="App">
+         <Provider store={store}>
+         <ToastContainer />
+
       <AppRouter/>
+      </Provider>
      </div>
   );
 }
